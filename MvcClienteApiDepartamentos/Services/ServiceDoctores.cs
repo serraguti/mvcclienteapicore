f27ca86081f269fbd0ad9252ea3890ec01a63217
serraguti <pacoserranox@gmail.com>
2021-03-04 13:19:23 +0100

    commit

---
+++ MvcClienteApiDepartamentos/Services/ServiceDoctores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MvcClienteApiDepartamentos.Services
+{
+    public class ServiceDoctores
+    {
+        private Uri UriApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceDoctores(String url)
+        {
+            this.UriApi = new Uri(url);
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+
+    }
+}
