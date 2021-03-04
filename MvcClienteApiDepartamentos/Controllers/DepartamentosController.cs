fdf2af4538838586c77f96fff996f9084110534d
serraguti <pacoserranox@gmail.com>
2021-03-04 09:19:12 +0100

    commit

---
+++ MvcClienteApiDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcClienteApiDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ApiClienteAjax()
+        {
+            return View();
+        }
+
+        public IActionResult ApiCrudServidor()
+        {
+            return View();
+        }
+    }
+}
