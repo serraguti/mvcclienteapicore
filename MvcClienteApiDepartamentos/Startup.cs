a93a6c898da91b1d1c26bd8acf68d70f68a729a3
serraguti <pacoserranox@gmail.com>
2021-03-05 10:04:08 +0100

    commit

--- MvcClienteApiDepartamentos/Startup.cs
+++ MvcClienteApiDepartamentos/Startup.cs
@@ -32,7 +32,6 @@ namespace MvcClienteApi
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
